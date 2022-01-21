ec29663f5fbe0139b88a315f7a5a48a51739aea0
AlinaNabi <amnabieva@gmail.com>
2022-01-22 01:21:26 +0300

    added

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,18 @@
+package domain;
+
+public class Post {
+    private String logoGroup;
+    private String nameGroup;
+    private String visitingTime;
+    private String text;
+    private String image;
+    private int counterLikePost;
+    private int counterRepost;
+    private int counterView;
+    private String likePost;
+    private String repost;
+    private String link;
+
+
+    // + get/set
+}
